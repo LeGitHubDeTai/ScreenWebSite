4023dcdb1c9cea8aa562d45349c0330da4e8e472
Tai Studio <tai.studio@outlook.fr>
2022-02-16 18:06:47 +0100

    update

--- main.js
+++ main.js
@@ -7,8 +7,19 @@ dotenv.config();
     if(process.env.websiteURL == null){
         process.env.websiteURL = 'https://tai-studio.netlify.app/';
     }
+    if(process.env.width != null){
+        process.env.width = 1920;
+    }
+    if(process.env.height != null){
+        process.env.height = 1080;
+    }
 
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+        defaultViewport: {
+          width:process.env.width,
+          height:process.env.height
+        }
+    });
     const page = await browser.newPage();
     await page.goto(process.env.websiteURL);
     await page.screenshot({ path: 'screen.png' });
